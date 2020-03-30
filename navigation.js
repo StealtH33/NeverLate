75d8d8822369544e7f0035889304aa844ff2c8ab
StealtH33 <st3alth364@gmail.com>
2020-03-31 00:11:03 +0300

    NeverLate Login/Register UI v1

---
+++ navigation.js
@@ -0,0 +1,29 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import Login from './screens/login';
+import Register from './screens/register';
+import Home from './screens/home';
+
+const screens ={
+     Login: {
+         screen: Login,
+         navigationOptions: {
+            headerShown: false
+          }
+     },
+     Register: {
+          screen: Register,
+          navigationOptions: {
+               headerShown: false
+          }
+     },
+     Home:{
+          screen: Home,
+          navigationOptions: {
+               headerShown: false
+          }
+     }
+} 
+
+const Navigator = createStackNavigator(screens);
+export default createAppContainer(Navigator);
